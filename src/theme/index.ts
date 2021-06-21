96b1800406d10cb0fbeeca177007302e237d6235
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-21 15:37:42 -0300

    Fix: Adjust ExtendTheme Props

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,20 +1,15 @@
 // // 1. Import the extendTheme function
-import { extendTheme, ThemeConfig } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
 
-import styles from './styles'
-import colors from './foundations/colors'
-import typography from "./foundations/typography"
+import styles from "./styles";
+import colors from "./foundations/colors";
+import typography from "./foundations/typography";
 
-const config: ThemeConfig = {
-    initialColorMode: "light"
-}
+// 2. Extend the theme to include custom colors, fonts, etc
+const theme = extendTheme({ 
+    styles: styles,
+    colors: colors,
+    ...typography,
+ });
 
-// // 2. Extend the theme to include custom colors, fonts, etc
-const theme = extendTheme({
-    ...styles,
-    config,
-    ...colors,
-    typography
-})
-
-export default theme
+export default theme;
