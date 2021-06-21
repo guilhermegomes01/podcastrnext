302661ad9aa6b61d135b10ffd55852f766abfdf8
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-21 15:40:03 -0300

    WIP: Create the container Player

---
+++ src/components/organisms/Player/index.tsx
@@ -0,0 +1,20 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+const Player = () => {
+  return (
+    <Flex
+      py="12"
+      px="16"
+      h="100vh"
+      bg="purple.500"
+      color="white"
+      direction="column"
+      align="center"
+      justify="space-between"
+      width="sm"
+    ></Flex>
+  );
+};
+
+export default Player;
