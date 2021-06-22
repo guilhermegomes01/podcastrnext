5e0f5cb4179282957bf6f099e0587eee49996abf
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-22 14:25:16 -0300

    Feat: Implement the AppTemplate in the app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import theme from "../theme"
+import { ChakraProvider } from "@chakra-ui/react";
+import Header from "../components/organisms/Header";
+import Player from "../components/organisms/Player";
+import AppTemplate from "../components/templates/AppTemplate";
+import theme from "../theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <AppTemplate AsideComponent={<Player />}>
+        <Header />
+        <Component {...pageProps} />
+      </AppTemplate>
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
