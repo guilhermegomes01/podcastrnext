9abfa91db9ceabca43f0c46f8197dc1066293a37
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-24 18:20:37 -0300

    Fix: Change name of component Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import Header from "../components/organisms/Header";
+import MainHeader from "../components/organisms/MainHeader";
 import Player from "../components/organisms/Player";
 import AppTemplate from "../components/templates/AppTemplate";
 import theme from "../theme";
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <AppTemplate AsideComponent={<Player />}>
-        <Header />
+        <MainHeader />
         <Component {...pageProps} />
       </AppTemplate>
     </ChakraProvider>
