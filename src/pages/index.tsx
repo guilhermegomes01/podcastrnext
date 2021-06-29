be13bd76dd4618b5532c94de541e47974106aa8b
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-29 10:26:49 -0300

    Feat: Integrate app with fake back-end

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React from "react";
+import { GetStaticProps } from "next";
 
-export default function Home() {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
+import { format, parseISO } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
+import api from "../services/api";
+import convertDurationToTimeString from "../utils/convertDurationToTimeString";
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  publishedAt: string;
+  duration: string;
+  description: string;
+  url: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home({ episodes }: HomeProps) {
+  return episodes.map((episode) => (
+    <div key={episode.id}>
+      <div>{episode.title}</div>
+      <hr />
+    </div>
+  ));
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _order: "",
+      _sort: "desc",
+    },
+  });
+
+  const episodes = data.map((episode) => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), "d MMM yy", {
+        locale: ptBR,
+      }),
+      duration: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    };
+  });
+
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 60 * 60 * 24,
+  };
+};
---
+++ src/utils/convertDurationToTimeString.ts
@@ -0,0 +1,11 @@
+export default function convertDurationToTimeString(duration: number) {
+    const hours = Math.floor(duration / 3600);
+    const minutes = Math.floor((duration % 3600) / 60);
+    const seconds = duration % 60;
+
+    const timeString = [hours, minutes, seconds]
+    .map(time => String(time).padStart(2, '0'))
+    .join(":")
+
+    return timeString
+}
