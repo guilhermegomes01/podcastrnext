dcb1c224ea89c7df1f83c4beeceaa0939d3c8b62
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-29 14:45:50 -0300

    Feat: Complete empty player section

--- src/components/organisms/Player/index.tsx
+++ src/components/organisms/Player/index.tsx
@@ -1,5 +1,14 @@
-import { Flex } from "@chakra-ui/react";
 import React from "react";
+
+import {
+  Flex,
+  Box,
+  Heading,
+  HStack,
+} from "@chakra-ui/react";
+
+import ButtonsActionPlayer from "../../molecules/ButtonsActionPlayer";
+import ProgressPlayer from "../../molecules/ProgressPlayer";
 
 const Player = () => {
   return (
@@ -7,13 +16,44 @@ const Player = () => {
       py="12"
       px="16"
       h="100vh"
-      bg="purple.500"
+      bgColor="purple.500"
       color="white"
       direction="column"
       align="center"
       justify="space-between"
       width="sm"
-    ></Flex>
+      as="aside"
+      data-testid="player-component"
+    >
+      <HStack as="header" spacing="4" align="center">
+        <img src="/playing.svg" alt="Tocando agora" />
+        <Heading as="h2" fontSize="md">
+          Tocando agora
+        </Heading>
+      </HStack>
+
+      {/* Empty Player */}
+      <Flex
+        h={80}
+        border="1.5px dashed"
+        borderColor="purple.300"
+        borderRadius="3xl"
+        bgGradient="linear(143.8deg, rgba(145, 100, 250, 0.8) 0%, rgba(0, 0, 0, 0)) 100%"
+        p="16"
+        textAlign="center"
+        align="center"
+        justify="center"
+      >
+        <Heading as="h2" fontSize="md">
+          Selecione um podcast para ouvir{" "}
+        </Heading>
+      </Flex>
+
+      <Box as="footer">
+        <ProgressPlayer />
+        <ButtonsActionPlayer />
+      </Box>
+    </Flex>
   );
 };
 
