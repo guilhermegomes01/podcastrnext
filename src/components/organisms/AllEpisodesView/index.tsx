0cb28f22de957c134e06599b12bb21b844d6c47a
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-06-30 14:59:31 -0300

    Feat: Create the section that show all episodes

---
+++ src/components/organisms/AllEpisodesView/index.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react';
+import React from 'react'
+
+const AllEpisodesView = () => {
+    return (
+        <Box as="section">
+            
+        </Box>
+    )
+}
+
+export default AllEpisodesView;
