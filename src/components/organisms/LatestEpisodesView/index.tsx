0db338f2586219ce33d106133227d97f47199181
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-07-01 11:12:32 -0300

    Fix: Style view of latest episodes

--- src/components/organisms/LatestEpisodesView/index.tsx
+++ src/components/organisms/LatestEpisodesView/index.tsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import { Box, Heading, List } from '@chakra-ui/react';
+import React from "react";
+import { Box, Heading, List } from "@chakra-ui/react";
 
-import { Episode } from '../../../pages';
-import LatestEpisode from '../../molecules/LatestEpisode';
+import { Episode } from "../../../pages";
+import LatestEpisode from "../../molecules/LatestEpisode";
 
 type LatestEpisodesViewProps = {
-    latestEpisodes: Episode[];
-}
+  latestEpisodes: Episode[];
+};
 
 const LatestEpisodesView = ({ latestEpisodes }: LatestEpisodesViewProps) => {
-    return (
-        <Box as="section">
-            <Heading as="h2">Últimos lançamentos</Heading>
+  return (
+    <Box as="section">
+      <Heading mt="12" mb="6" as="h2" fontSize="2xl" color="gray.800">
+        Últimos lançamentos
+      </Heading>
 
-            <List>
-                {latestEpisodes.map(episode => (
-                    <LatestEpisode key={episode.id} episode={episode} />
-                ))}
-            </List>
-        </Box>
-    )
-}
+      <List display="grid" gridTemplateColumns="repeat(2, 1fr)" gridGap="6">
+        {latestEpisodes.map((episode) => (
+          <LatestEpisode key={episode.id} episode={episode} />
+        ))}
+      </List>
+    </Box>
+  );
+};
 
 export default LatestEpisodesView;
