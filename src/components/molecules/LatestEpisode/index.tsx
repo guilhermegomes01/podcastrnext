49b6f91a35678eeb4f6782fc180d1d784ea2aa98
Guilherme Gomes <guilherme.glima@stone.com.br>
2021-07-01 11:12:07 -0300

    Fix: Style the card that show info about last episode

--- src/components/molecules/LatestEpisode/index.tsx
+++ src/components/molecules/LatestEpisode/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from 'next/image'
+import Image from "next/image";
 
 import { Box, IconButton, Link, ListItem, Text } from "@chakra-ui/react";
 import { IoPlay } from "react-icons/io5";
@@ -9,10 +9,15 @@ type LatestEpisodeProps = {
   episode: Episode;
 };
 
+const spanStyles = {
+  display: "inline-block",
+  mt: "2",
+  fontSize: "sm",
+};
+
 const LatestEpisode = ({
   episode: {
     members,
-    description,
     duration,
     publishedAt,
     thumbnail,
@@ -20,21 +25,73 @@ const LatestEpisode = ({
     url,
   },
 }: LatestEpisodeProps) => (
-  <ListItem>
-    <Image width={192} height={192} src={thumbnail} alt={title} objectFit="cover" />
+  <ListItem
+    bg="white"
+    border="1px solid"
+    borderColor="gray.100"
+    p="5"
+    borderRadius="2xl"
+    position="relative"
+    display="flex"
+    alignItems="center"
+  >
+    <Box boxSize="28">
+      <Image
+        width={192}
+        height={192}
+        src={thumbnail}
+        alt={title}
+        objectFit="cover"
+      />
+    </Box>
 
-    <Box>
-      <Link href={url}>{title}</Link>
-      <Text>{members}</Text>
-      <Text>{publishedAt}</Text>
-      <Text>{duration}</Text>
+    <Box flex="1" ml="4" className="episodeDetails">
+      <Link
+        color="gray.800"
+        fontFamily="Lexend, sans-serif"
+        fontWeight="semibold"
+        lineHeight="6"
+        href={url}
+      >
+        {title}
+      </Link>
+      <Text mt="2" maxW="70%" fontSize="sm" isTruncated>
+        {members}
+      </Text>
+      <Text
+        as="span"
+        mr="2"
+        pr="2"
+        position="relative"
+        _after={{
+          content: '""',
+          boxSize: "4px",
+          borderRadius: "50%",
+          background: "gray.100",
+          position: 'absolute',
+          right: 0,
+          top: '50%',
+          transform: "translate(50%, -25%)"
+        }}
+        {...spanStyles}
+      >
+        {publishedAt}
+      </Text>
+      <Text as="span" {...spanStyles}>
+        {duration}
+      </Text>
     </Box>
 
     <IconButton
       aria-label="Tocar episódio"
       icon={<IoPlay />}
-      colorScheme="whatsapp"
+      color="green"
       variant="outline"
+      position="absolute"
+      right="8"
+      bottom="8"
+      borderColor="gray.100"
+      borderRadius="xl"
     />
   </ListItem>
 );
